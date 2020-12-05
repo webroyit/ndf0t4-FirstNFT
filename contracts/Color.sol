7f0cb17f73bedfa8c3e811114ffdd8902c8555a6
royweb <xytank@gmail.com>
2020-12-05 14:19:04 -0500

    Add a function to create a new unique token

--- contracts/Color.sol
+++ contracts/Color.sol
@@ -3,6 +3,23 @@ pragma solidity >=0.4.21 <0.7.0;
 import "./ERC721Full.sol";
 
 contract Color is ERC721Full {
-  constructor() ERC721Full("Color", "COLOR") public {
+  string[] public colors;
+  mapping(string => bool) _colorExists;   // For checking that the color is unique
+
+  constructor() ERC721Full("Color", "COLOR") public {}
+
+  // Create a new unique token
+  function mint(string memory _color) public {
+    // Require unique color
+    require(!_colorExists[_color]);
+
+    // Add new color to the array
+    uint _id = colors.push(_color);
+
+    // Call _mint() from ERC721Full
+    _mint(msg.sender, _id);
+
+    // Color - track it and add it
+    _colorExists[_color] = true;
   }
 }
--- test/Color.test.js
+++ test/Color.test.js
@@ -32,4 +32,42 @@ contract('Color', (accounts) => {
             assert.equal(symbol, 'COLOR');
         })
     })
+
+    describe('minting', async () => {
+        it('create a new token', async () => {
+            const result = await contract.mint('#42f560');
+            const totalSupply = await contract.totalSupply();
+            
+            // Success
+            assert.equal(totalSupply, 1);
+            const event = result.logs[0].args;
+            assert.equal(event.tokenId.toNumber(), 1, 'id is correct');
+            assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'from is correct');
+            assert.equal(event.to, accounts[0], 'to is correct');
+
+            // Failure: cannot mint same color twice
+            await contract.mint('#42f560').should.be.rejected;
+        })
+    })
+
+    describe('indexing', async () => {
+        it('lists colors', async () => {
+            // Mint 3 more tokens
+            await contract.mint('#70706e');
+            await contract.mint('#bf2a98');
+            await contract.mint('#8f7f3f');
+            const totalSupply = await contract.totalSupply();
+            
+            let color;
+            let result = [];
+
+            for (let i = 1; i <= totalSupply; i++) {
+                color = await contract.colors(i - 1);
+                result.push(color);
+            }
+
+            let expected = ['#42f560', '#70706e', '#bf2a98', '#8f7f3f'];
+            assert.equal(result.join(','), expected.join(','));
+        })
+    })
 })
